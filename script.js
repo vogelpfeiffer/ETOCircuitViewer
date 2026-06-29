803aa5fb7765cb43e4995fc8dba64d027672d000
R Marques <r.piffr@gmail.com>
2026-06-29 10:17:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const map = L.map('map').setView([-10.25, -48.33], 10);
 
 L.tileLayer(
-'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-{
-    maxZoom:19,
-    attribution:'© OpenStreetMap'
-}
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap'
+    }
 ).addTo(map);
