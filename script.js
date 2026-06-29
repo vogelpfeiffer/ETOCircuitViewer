dd37584dfab3ecf7844b260c0ebbb4e17e1e6959
R Marques <r.piffr@gmail.com>
2026-06-29 10:10:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("ETO Circuit Viewer loaded.");
+const map = L.map('map').setView([-10.25, -48.33], 10);
+
+L.tileLayer(
+'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+{
+    maxZoom:19,
+    attribution:'© OpenStreetMap'
+}
+).addTo(map);
