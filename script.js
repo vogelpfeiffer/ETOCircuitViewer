416bbb0e33cfde46ca2bc16da94accbea4898ddd
R Marques <r.piffr@gmail.com>
2026-06-30 10:23:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-const map = L.map('map').setView([-10.25,-48.33],10);
+console.log("Script started");
+
+const map = L.map('map').setView([-10.25, -48.33], 10);
 
 L.tileLayer(
-'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-{
-maxZoom:19,
-attribution:'© OpenStreetMap'
-}
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+        attribution: '© OpenStreetMap',
+        maxZoom: 19
+    }
 ).addTo(map);
 
-
-// Load one KML
+console.log("Map created");
 
 const layer = omnivore.kml('kml/Norte/SE-2AGT_Circ-AL01014045.kml');
 
-layer.on('ready',function(){
+layer.on('ready', function () {
+    console.log("KML loaded");
+    map.fitBounds(layer.getBounds());
+});
 
-map.fitBounds(layer.getBounds());
-
+layer.on('error', function(e){
+    console.error("KML ERROR:", e);
 });
 
 layer.addTo(map);
