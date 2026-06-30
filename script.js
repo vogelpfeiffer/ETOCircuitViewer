b153e58cb1cefca6137037c5446946fca395d0cc
R Marques <r.piffr@gmail.com>
2026-06-30 10:08:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-const map = L.map('map').setView([-10.25, -48.33], 10);
+const map = L.map('map').setView([-10.25,-48.33],10);
 
 L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    {
-        maxZoom: 19,
-        attribution: '© OpenStreetMap'
-    }
+'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+{
+maxZoom:19,
+attribution:'© OpenStreetMap'
+}
 ).addTo(map);
+
+
+// Load one KML
+
+const layer = omnivore.kml('kml/SE-2AGT_Circ-AL01014045.kml');
+
+layer.on('ready',function(){
+
+map.fitBounds(layer.getBounds());
+
+});
+
+layer.addTo(map);
